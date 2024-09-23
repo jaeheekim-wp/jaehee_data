c6910ac3b028cceea91685e20b29ed6a6676bfd2
Jaehee Kim <kimjaehee9808@gmail.com>
2024-09-23 23:55:41 +0900

    second test

--- code/sesac_03.py
+++ code/sesac_03.py
@@ -1,2 +1,11 @@
 a=1
 a
+
+import numpy as np
+a = np.array([1, 2, 3, 4, 5]) # 숫자형 벡터 생성
+b = np.array(["apple", "banana", "orange"]) # 문자형 벡터 생성
+c = np.array([True, False, True, True]) # 논리형 벡터 생성
+print("Numeric Vector:", a)
+print("String Vector:", b)
+print("Boolean Vector:", c)
+type(a)
