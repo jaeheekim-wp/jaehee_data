58a637a0c84bdc89aa44e12449092a038d610876
Jaehee Kim <kimjaehee9808@gmail.com>
2024-09-24 22:04:13 +0900

    loc

--- code/sesac_03.py
+++ code/sesac_03.py
@@ -13,3 +13,51 @@ type(a)
 import pandas as pd
 df =  pd.read_csv('data/scores.csv')
 df
+
+# 시리즈
+df['name']
+df.name
+# 데이터프레임
+df[['name']]
+df.index
+df.shape
+df[["name","eng"]]
+
+# 인덱스/ 컬럼명으로 추출
+
+df.index = 'i' + df.index.astype("str")
+df.head()
+
+s1 = df.loc["i3"]
+s1.values
+# df.loc[0] 인덱스가 문자열일때는 번호로 뽑을 수 없음 .
+
+df.loc[['i1','i3','i5']]
+
+# 행열 같이 추출
+df.loc["i1",'kor']
+type(df.loc["i1",'kor'])
+
+# 열 여러개
+df.loc["i1",["name","kor"]]
+# 행 여러개
+df.loc[["i1","i2","i3"],"name"]
+# 행/열 여러개
+df.loc[["i1","i2","i3"],["name","kor"]]
+# 모든 행에서 컬럼 추출
+df.loc[:,"name"] # 시리즈
+df.loc[:,["name"]] # 데이터프레임
+df.loc[:,["name","math"]]
+
+# ========
+# iloc 사용
+
+df.iloc[0]
+df.iloc[[1,3,5]]
+
+# 슬라이싱 추출
+df.iloc[0:4]
+# 1,3,5 행 추출
+df.iloc[1:6:2]
+df.iloc[1:2]
+
